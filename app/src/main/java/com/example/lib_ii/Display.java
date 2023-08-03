8ae28d7c22e10960149fe049173d9351f468a063
jamshaid <jam172002@gmail.com>
2023-08-03 18:54:55 +0500

    Revert "comments in Display.java files are done"
    This reverts commit 24af88c8d18bb155e1ed4460ff78f794fe3280db.

--- app/src/main/java/com/example/lib_ii/Display.java
+++ app/src/main/java/com/example/lib_ii/Display.java
@@ -12,30 +12,20 @@ import com.example.lib_ii.databinding.ActivityDisplayBinding;
 
 import java.util.ArrayList;
 
-// The Display class is an Activity that displays a list of books using RecyclerView.
+
 public class Display extends AppCompatActivity {
 
-    // View binding for the activity
     ActivityDisplayBinding bind;
-
-    // ArrayList to hold book data
     ArrayList<Model> arrData = new ArrayList<>();
-
-    // DBHelper instance to interact with the database
     DBHelper db;
-
-    // Adapter for the RecyclerView
     bookAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Inflate the layout using view binding
         bind = ActivityDisplayBinding.inflate(getLayoutInflater());
         setContentView(bind.getRoot());
 
-        // Set an OnClickListener to the "Add" button to navigate to the Add activity
+        //add button taking to add activity
         bind.btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,17 +33,22 @@ public class Display extends AppCompatActivity {
             }
         });
 
-        // RecyclerView setup
+       /* arrData.add(new Model(122, "Biology", "PCTB", "300"));
+        arrData.add(new Model(26, "Chemistry", "PCTB", "250"));
+        arrData.add(new Model(5, "Physics", "PCTB", "350"));*/
+
         bind.reV.setLayoutManager(new LinearLayoutManager(this));
+
         adapter = new bookAdapter(this, arrData);
+
         bind.reV.setAdapter(adapter);
 
-        // Database work
-        // Create a new instance of DBHelper to interact with the database
-        db = new DBHelper(Display.this);
 
-        // Retrieve all book data from the database and display it in the Log
+
+        //Database work now
+        db = new DBHelper(Display.this);
         ArrayList<Model> arr = db.getData();
+
         for (int i = 0; i < arr.size(); i++) {
             Log.d("book info", "id: " + arr.get(i).getM_id() +
                     ", name: " + arr.get(i).getM_name() +
@@ -61,19 +56,20 @@ public class Display extends AppCompatActivity {
                     ", pages: " + arr.get(i).getM_page());
         }
 
-        // Retrieve all book data from the database and set it to the RecyclerView
+
         arrData = db.getData();
         adapter.setData(arrData);
-    }
 
+
+    }
     @Override
     protected void onResume() {
         super.onResume();
 
-        // When the activity resumes, retrieve new data from the database
-        // and update the RecyclerView adapter with the new data
+        // Retrieve new data from the database when the activity resumes
         ArrayList<Model> newData = db.getData();
         adapter.setData(newData);
         adapter.notifyDataSetChanged();
     }
+
 }
